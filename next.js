31d499cf99a2b73dacabaad481353ff0e68796d1
URADI KIRAN KUMAR <101377115+kiranuradi@users.noreply.github.com>
2023-07-07 14:56:24 +0530

    Add files via upload

---
+++ next.js
@@ -0,0 +1,25 @@
+
+function change1() {
+    var img = document.getElementById('image3a');
+    document.getElementById('subhead3').innerHTML = 'Remote Sensing'; 
+    document.getElementById('matter3').innerHTML = 'With in-house experts in Remote sensing, image interpretation and Classification, you can rely on us to implement innovative techniques to get precise land cover classifications for a variety of applications.';
+    img.src = 'img3.jpg';
+}
+function change2() {
+    var img = document.getElementById('image3a');
+    document.getElementById('subhead3').innerHTML = 'GIS Data Management'; 
+    document.getElementById('matter3').innerHTML = "Our results-driven culture follows a data first policy, helping identify and assess key data sources specific to project, and assist in data Collection, processing, creating, modeling, cleaning and validating, and data migration services.";
+    img.src = 'm3.jpg';
+}
+function change3() {
+    var img = document.getElementById('image3a');
+    document.getElementById('subhead3').innerHTML = 'Customized GIS Solutions'; 
+    document.getElementById('matter3').innerHTML = "We offer customized GIS solutions for desktops, web, cloud, and mobile applications along with seamless integration across platforms and verticals to build revolutionary solutions varying from one use case to another, as you go.";
+    img.src = 'm4.jpg';
+}
+function change() {
+    var img = document.getElementById('image3a');
+    document.getElementById('subhead3').innerHTML = 'GIS Services'; 
+    document.getElementById('matter3').innerHTML = "Our forefront knowledge of data mapping, feature extraction / compilation, information management, and integrating verticals with GIS expertise helps build application-centric solutions for various sectors.";
+    img.src = 'm1.jpg';
+}
